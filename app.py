528e916d44c19010352732cdb7ea82279987328c
Pavan-7673 <annapareddypavanreddy0@gmail.com>
2026-03-26 19:06:53 +0530

    Fix updated

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "CI/CD Working!"
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=5000)
