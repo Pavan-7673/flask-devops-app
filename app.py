681b056daae1df4da4a969df7cd4379a41bb4874
Pavan-7673 <annapareddypavanreddy0@gmail.com>
2026-03-26 19:14:45 +0530

    Resolves merge

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ app = Flask(__name__)
 def home():
     return "CI/CD Working!"
 if __name__ == "__main__":
-<<<<<<< HEAD
    app.run(host="0.0.0.0", port=5000)
-=======
-app.run(host="0.0.0.0", port=5000)
->>>>>>> 021683cfcf54a1e27d3db0be59a18fb8c930cc92
+
